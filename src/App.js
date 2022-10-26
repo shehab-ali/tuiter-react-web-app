a90cd1255e706325fdfb098ddc1edf1a095f5482
shehab-ali <shehabali1125@gmail.com>
2022-10-26 16:58:33 -0400

    lab 7

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import Labs from "./labs";
 import HelloWorld from "./labs/a6/hello-world";
 import Tuiter from "./tuiter";
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import { Routes, Route } from "react-router";
+import {BrowserRouter} from "react-router-dom";
+import {Routes, Route} from "react-router";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <div className="container">
-                <Routes>
-                    <Route index element={<Labs />}></Route>
-                    <Route path="/hello" element={<HelloWorld />}></Route>
-                    <Route path="/tuiter" element={<Tuiter />}></Route>
-                </Routes>
-            </div>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <div className="container">
+      <Routes>
+        <Route path="/*" element={<Labs />}/>
+        <Route path="/hello" element={<HelloWorld/>}/>
+        <Route path="/tuiter/*" element={<Tuiter/>}/>
+      </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 export default App;
